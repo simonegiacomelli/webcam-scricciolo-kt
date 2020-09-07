9e26f301b4e45dcf227fb15743af1ac71d22f620
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-09-07 15:54:27 +0200

    implemented delete

---
+++ src/jsMain/kotlin/EventShow.kt
@@ -0,0 +1,116 @@
+import kotlinx.browser.window
+import org.w3c.dom.HTMLButtonElement
+import kotlin.math.ceil
+
+class EventShow(val day: ApiDay, val event: ApiEvent, val btn: HTMLButtonElement) {
+
+    companion object {
+        var show: EventShow? = null
+        fun setCurrentShowTo(s: EventShow) {
+            show = s
+        }
+    }
+
+    var handle: Int? = null
+    var imageIndex = -1
+    var files: List<String> = emptyList()
+    var allFiles: List<String> = emptyList()
+
+    suspend fun buttonClick() {
+        println("eventButtonClick")
+        setCurrentShowTo(this)
+        setupEvents()
+
+        allFiles = api.New(EventRequest(event.firstFileName)).files
+        updateFiles()
+        fromBeginning()
+    }
+
+    private fun fromBeginning() {
+        imageIndex = -1
+        nextClick()
+    }
+
+    private fun updateFiles() {
+        files = if (page.maskEnabled) allFiles else allFiles.filter { !it.endsWith("m.jpg") }
+    }
+
+    fun tick() {
+        if (show != this) {
+            println("Show changed! was ${event.time}")
+            return
+        }
+        if (!page.automaticNext)
+            return
+
+        if (showImage(1))
+            setTimeout()
+    }
+
+    private fun nextClick() {
+        showImage(1)
+        setTimeout()
+    }
+
+    private fun showImage(offset: Int): Boolean {
+
+        val idx = imageIndex + offset
+        if (idx < 0 || idx >= files.size)
+            return false
+        imageIndex = idx
+
+        val filename = files[idx]
+        val fullName = "${event.dayFolder}/${event.name}/$filename"
+        page.img.src = "/image?full_filename=$fullName"
+        page.progressbar.value = ceil((imageIndex + 1).toDouble() / files.size * 100)
+        page.imgDiv.innerHTML = "${event.time} ${imageIndex + 1}/${files.size}"
+        debugLoad(fullName, idx)
+        return true
+    }
+
+    private fun debugLoad(fullName: String, idx: Int) {
+        console.log("$fullName Loading")
+        page.img.onload = { console.log("$fullName Loaded OK ${idx + 1}/${files.size}") }
+    }
+
+    private fun setupEvents() {
+        page.progressbar.apply {
+            onclick = fun(e) {
+                val valueClicked = e.offsetX * this.max / this.offsetWidth / 100;
+                val x = files.size * valueClicked
+                imageIndex = ceil(x).toInt() - 1
+                println("valueClicked=$valueClicked x=$x imageIndex=$imageIndex")
+                showImage(0)
+            }
+        }
+
+        page.prevBtn.onclick = { showImage(-1) }
+        page.nextBtn.onclick = { nextClick() }
+        page.maskCheckBox.onclick = { fromBeginning() }
+        page.resetBtn.onclick = { fromBeginning() }
+        page.deleteBtn.onclickExt = { deleteEvent() }
+    }
+
+    private suspend fun deleteEvent() {
+        if (!window.confirm("Are you sure?"))
+            return
+        clearTimeout()
+        api.New(ApiDeleteEvent(event.firstFileName))
+        btn.remove()
+    }
+
+
+    private fun setTimeout() {
+        clearTimeout()
+        if (!page.automaticNext)
+            return
+        handle = window.setTimeout({ tick() }, page.intervalMsec)
+    }
+
+    private fun clearTimeout() {
+        val h = handle ?: return
+        window.clearTimeout(h)
+        handle = null
+    }
+
+}
