1a9d6eed40555fbf226cf954817ce95921ed1972
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-09-07 08:19:16 +0200

    many test cases

--- src/jvmMain/kotlin/Webcam.kt
+++ src/jvmMain/kotlin/Webcam.kt
@@ -1,12 +1,21 @@
 import java.io.File
 
-data class Group(val name: String)
-class Webcam(val pathname: File) {
+data class Event(val root: File) {
+    val firstFile: File = root.listFiles().filterNotNull().minByOrNull { it.name }!!
+    val name: String = root.name
+    val time: String = firstFile.name.split("-")[1].substring(8).chunked(2).joinToString(":")
+}
+
+data class Day(val root: File) {
+    val events = list(root).map { Event(it) }
+    val name: String get() = root.name
+}
+
+class Webcam(private val root: File) {
     constructor(pathname: String) : this(File(pathname))
 
-    fun listGroups(): List<Group> {
+    val days = list(root).map { Day(it) }
 
-        val f = pathname.list().filterNotNull().sorted().map { Group(it) }
-        return f
-    }
 }
+
+private fun list(file: File) = file.listFiles().filterNotNull().sorted()
--- src/jvmTest/kotlin/WebcamTest.kt
+++ src/jvmTest/kotlin/WebcamTest.kt
@@ -1,14 +1,47 @@
-import java.io.File
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertSame
 import kotlin.test.fail
 
 internal class WebcamTest {
+    val pathname = "./src/jvmTest/resources/test_files/flat_files"
 
     @Test
-    fun listGroups() {
-        val pathname = "./src/jvmTest/resources/test_files/flat_files"
+    fun listDays() {
         val target = Webcam(pathname)
-        assertEquals(listOf("20200825", "20200827", "20200830"), target.listGroups().map { it.name })
+        assertEquals(listOf("20200825", "20200827", "20200830"), target.days.map { it.name })
     }
+
+    @Test
+    fun listDay_events() {
+        val target = Webcam(pathname)
+        val day = target.days.first { it.name == "20200830" }
+        assertEquals(listOf("804", "805"), day.events.map { it.name })
+    }
+
+    @Test
+    fun listDay_days_twiceCalled_shouldBeSameReference() {
+        val target = Webcam(pathname)
+        assertSame(target.days, target.days)
+    }
+
+    @Test
+    fun listDay_events_twiceCalled_shouldBeSameReference() {
+        val target = Webcam(pathname)
+        assertSame(target.days.first().events, target.days.first().events)
+    }
+
+    @Test
+    fun list_event_time() {
+        val target = Webcam(pathname)
+        val day = target.days.first { it.name == "20200830" }
+        val event = day.events.associateBy { it.name }
+        val e804 = event["804"] ?: fail("Should exist")
+        assertEquals("CAM1_804-20200830051609-01.jpg", e804.firstFile.name)
+        assertEquals("05:16:09", e804.time)
+        val e805 = event["805"] ?: fail("Should exist")
+        assertEquals("CAM1_805-20200830051636-01.jpg", e805.firstFile.name)
+        assertEquals("05:16:36", e805.time)
+    }
+
 }
